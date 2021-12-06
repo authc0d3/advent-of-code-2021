8f769fd75af5e636dd17777f8dba2676ce209223
authcode <agonzalezg86@gmail.com>
2021-12-06 15:50:57 +0100

    refactor: add all read file option into an object

---
+++ src/common/types.d.ts
@@ -0,0 +1,5 @@
+export type ReadFileOptions = {
+  encoding?: BufferEncoding;
+  splitLines?: boolean;
+  ignoreBlankLines?: boolean;
+};
--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,13 +1,25 @@
 import * as fs from "fs";
+import { ReadFileOptions } from "./types";
+
+export function removeBlankLines(data: string[]): string[] {
+  return data.filter((line) => line !== "");
+}
 
 export function readFile(
   filePath: string,
-  encoding: BufferEncoding = "utf8",
-  splitLines: boolean = true
+  options?: ReadFileOptions
 ): string | string[] {
+  const encoding = options?.encoding ?? "utf8";
+  const splitLines = options?.splitLines ?? true;
+  const ignoreBlankLines = options?.ignoreBlankLines ?? true;
   try {
-    const data = fs.readFileSync(filePath, encoding);
-    return splitLines ? data.replace(/\r/gi, "").split("\n") : data;
+    const data = fs.readFileSync(filePath, encoding || "utf-8");
+    return splitLines
+      ? data
+          .replace(/\r/gi, "")
+          .split("\n")
+          .filter((line) => ignoreBlankLines || line !== "")
+      : data;
   } catch (err) {
     console.error(err);
     return splitLines ? [] : "";
